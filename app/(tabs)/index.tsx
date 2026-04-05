f1cc057fa566e7fac0cb53ced3f3e50b4c940327
zZzRaMmOzZz <meraachlym@gmail.com>
2026-04-05 16:36:32 +0300

    implements fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,13 +11,13 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+      <Text className="font-sans-extrabold text-primary text-5xl ">
+        Home
       </Text>
-      <Link href="/onboarding" className="mt-4 bg-primary text-white p-4">Go to Onboarding</Link>
+      <Link href="/onboarding" className="mt-4 font-sans-medium  bg-primary text-white p-4">Go to Onboarding</Link>
     
-      <Link href="/(auth)/sign-in" className="mt-4 bg-primary text-white p-4">Go to Sign In</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 bg-primary text-white p-4">Go to Sign Up</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-medium bg-primary text-white p-4">Go to Sign In</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-medium bg-primary text-white p-4">Go to Sign Up</Link>
     
     </SafeAreaView>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,28 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-bold": require("@/assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-extrabold": require("@/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-medium": require("@/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-regular": require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-semibold": require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-light": require("@/assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{headerShown: false}}/>;
 }
