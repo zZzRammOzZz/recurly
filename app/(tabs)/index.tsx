086af50585145c2565c2bd130c7bf88e9ab3d61f
zZzRaMmOzZz <meraachlym@gmail.com>
2026-04-05 16:19:28 +0300

    seccond commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
- 
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
+
+
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -13,18 +19,6 @@ export default function App() {
       <Link href="/(auth)/sign-in" className="mt-4 bg-primary text-white p-4">Go to Sign In</Link>
       <Link href="/(auth)/sign-up" className="mt-4 bg-primary text-white p-4">Go to Sign Up</Link>
     
-    <Link href={{
-      pathname: "/(tabs)/subscriptions/[id]",
-      params: { id: "1" },
-    }} className="mt-4 bg-primary text-white p-4">Go to Subscription Spotify</Link>
-    <Link href={{
-      pathname: "/(tabs)/subscriptions/[id]",
-      params: { id: "2" },
-    }} className="mt-4 bg-primary text-white p-4">Go to Subscription Netflix</Link>
-    <Link href={{
-      pathname: "/(tabs)/subscriptions/[id]",
-      params: { id: "3" },
-    }} className="mt-4 bg-primary text-white p-4">Go to Subscription Disney+</Link>
-    </View>
+    </SafeAreaView>
   );
 }
